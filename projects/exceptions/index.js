33707c0aa6c392ef6e8c1118e87472bdec806d61
Evgeny Blinov <sharppoint@yandex.ru>
2023-01-20 00:26:54 +0300

    Example #3 Exceptions & Dwebug

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -16,7 +16,24 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
-function isAllTrue(array, fn) {}
+function isAllTrue(array, fn) {
+  if (!Array.isArray(array)) {
+    throw new Error('empty array');
+  } else {
+    if (array.length === 0) {
+      throw new Error('empty array');
+    }
+  }
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+  for (const item of array) {
+    if (!fn(item)) {
+      return false;
+    }
+  }
+  return true;
+}
 
 /*
  Задание 2:
@@ -34,7 +51,24 @@ function isAllTrue(array, fn) {}
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array, fn) {
+  if (!Array.isArray(array)) {
+    throw new Error('empty array');
+  } else {
+    if (array.length === 0) {
+      throw new Error('empty array');
+    }
+  }
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+  for (const item of array) {
+    if (fn(item)) {
+      return true;
+    }
+  }
+  return false;
+}
 
 /*
  Задание 3:
@@ -47,7 +81,20 @@ function isSomeTrue(array, fn) {}
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn, ...args) {}
+function returnBadArguments(fn, ...args) {
+  const throwCount = [];
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+  args.forEach((item) => {
+    try {
+      fn(item);
+    } catch (err) {
+      throwCount.push(item);
+    }
+  });
+  return throwCount;
+}
 
 /*
  Задание 4:
@@ -66,7 +113,23 @@ function returnBadArguments(fn, ...args) {}
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator(number = 0) {}
+function calculator(number = 0) {
+  console.log('Number: ', number);
+  if (isNaN(number)) {
+    throw new Error('number is not a number');
+  }
+  return {
+    sum: (...args) => args.reduce((prev, cur) => prev + cur, number),
+    dif: (...args) => number - args.reduce((acc, item) => acc + item, 0),
+    div: (...args) => {
+      if (args.some((item) => item === 0)) {
+        throw new Error('division by 0');
+      }
+      return args.reduce((acc, item) => acc / item, number);
+    },
+    mul: (...args) => args.reduce((acc, item) => acc * item, number),
+  };
+}
 
 /* При решении задач, постарайтесь использовать отладчик */
 
