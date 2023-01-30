f8aedc81a7566c7e62c14fe7a84967eee5b6b01e
Evgeny Blinov <sharppoint@yandex.ru>
2023-01-30 22:27:06 +0300

    example 6-dnd

--- projects/dnd/functions.js
+++ projects/dnd/functions.js
@@ -8,7 +8,9 @@
  Пример:
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
-function addListener(eventName, target, fn) {}
+function addListener(eventName, target, fn) {
+  target.addEventListener(eventName, fn);
+}
 
 /*
  Задание 2:
@@ -18,7 +20,9 @@ function addListener(eventName, target, fn) {}
  Пример:
    removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
  */
-function removeListener(eventName, target, fn) {}
+function removeListener(eventName, target, fn) {
+  target.removeEventListener(eventName, fn);
+}
 
 /*
  Задание 3:
@@ -28,7 +32,11 @@ function removeListener(eventName, target, fn) {}
  Пример:
    skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
-function skipDefault(eventName, target) {}
+function skipDefault(eventName, target) {
+  target.addEventListener(eventName, (e) => {
+    e.preventDefault();
+  });
+}
 
 /*
  Задание 4:
@@ -38,7 +46,9 @@ function skipDefault(eventName, target) {}
  Пример:
    emulateClick(document.querySelector('a')) // для указанного элемента должно быть симулировано события click
  */
-function emulateClick(target) {}
+function emulateClick(target) {
+  target.dispatchEvent(new Event('click'));
+}
 
 /*
  Задание 6:
@@ -49,7 +59,16 @@ function emulateClick(target) {}
  Пример:
    delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
-function delegate(target, fn) {}
+function delegate(target, fn) {
+  target.addEventListener('click', (e) => {
+    switch (e.target.tagName) {
+      case 'BUTTON': {
+        fn();
+        break;
+      }
+    }
+  });
+}
 
 /*
  Задание 7:
@@ -60,6 +79,16 @@ function delegate(target, fn) {}
  Пример:
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
-function once(target, fn) {}
+
+// function once(target, fn) {
+//   target.addEventListener('click', fn, { once: true });
+// }
+function once(target, fn) {
+  function removerEvent() {
+    target.removeEventListener('click', removerEvent);
+    fn.apply(this, arguments);
+  }
+  target.addEventListener('click', removerEvent);
+}
 
 export { addListener, removeListener, skipDefault, emulateClick, delegate, once };
--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,63 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+/* --------------------------------------------------------- */
+function configRandomDiv(id, size, position, color) {
+  const div = document.createElement('div');
+  div.classList.add('draggable-div');
+  div.setAttribute('draggable', 'true');
+  div.setAttribute('data-id', `${id}`);
+  div.style.position = 'absolute';
+  div.style.top = `${position.y}px`;
+  div.style.left = `${position.x}px`;
+  div.style.background = `rgba(${color.r},${color.g},${color.b},${color.a})`;
+  div.style.width = `${size.width}px`;
+  div.style.height = `${size.height}px`;
+  return div;
+}
+function fabricRandomDiv() {
+  const id = Math.round(Math.random() * 10e3);
+  const size = {
+    width: Math.round(Math.random() * 200) + 10,
+    height: Math.round(Math.random() * 200) + 10,
+  };
+  const position = {
+    x: Math.round(Math.random() * 600),
+    y: Math.round(Math.random() * 300),
+  };
+  const color = {
+    r: Math.round(Math.random() * 255),
+    g: Math.round(Math.random() * 255),
+    b: Math.round(Math.random() * 255),
+    a: Math.random(),
+  };
 
-export function createDiv() {}
+  return configRandomDiv(id, size, position, color);
+}
+
+function moveRandomDiv(e) {
+  const targetDiv = e.target;
+  targetDiv.style.left = e.pageX - targetDiv.offsetWidth / 2 + 'px';
+  targetDiv.style.top = e.pageY - targetDiv.offsetHeight / 2 + 'px';
+}
+
+// document.addEventListener('mousemove', moveRandomDiv);
+document.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  const div = e.target;
+  if (div.tagName === 'DIV' && div.classList.contains('draggable-div')) {
+    document.addEventListener('mousemove', moveRandomDiv);
+    div.addEventListener('mouseup', () => {
+      document.removeEventListener('mousemove', moveRandomDiv);
+    });
+  }
+});
+
+/* ---------------------------------------------- */
+
+export function createDiv() {
+  return fabricRandomDiv();
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
