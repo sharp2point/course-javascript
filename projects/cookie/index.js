2e77985c350b8da11e95792e05302036b58e72f8
Evgeny Blinov <sharppoint@yandex.ru>
2023-01-30 22:39:39 +0300

    fix webpack

--- projects/cookie/index.js
+++ projects/cookie/index.js
@@ -37,9 +37,9 @@ const homeworkContainer = document.querySelector('#app');
 // текстовое поле для фильтрации cookie
 const filterNameInput = homeworkContainer.querySelector('#filter-name-input');
 // текстовое поле с именем cookie
-const addNameInput = homeworkContainer.querySelector('#add-name-input');
+//const addNameInput = homeworkContainer.querySelector('#add-name-input');
 // текстовое поле со значением cookie
-const addValueInput = homeworkContainer.querySelector('#add-value-input');
+//const addValueInput = homeworkContainer.querySelector('#add-value-input');
 // кнопка "добавить cookie"
 const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
